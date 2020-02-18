3a55014c70ef503568651c984bbe910252e6438c
LuizWalker <53380246+LuizWalker@users.noreply.github.com>
2020-02-18 10:25:43 -0300

    Add files via upload

---
+++ testbench.vhd
@@ -0,0 +1,26 @@
+entity testbench is
+end testbench;
+
+architecture test of testbench is
+
+component testevetores is
+
+port(a: in bit_vector(5 downto 0);
+     y: out bit_vector(5 downto 0));
+	  
+end component;
+
+signal entrada: bit_vector(5 downto 0);
+signal saida: bit_vector(5 downto 0);
+
+begin
+
+dut: testevetores port map(entrada,saida);
+
+entrada <= "000000",
+           "001010" after 10 ns,
+			  "110100" after 20 ns,
+           "111001" after 30 ns,
+           "000000" after 40 ns;
+			  
+end test;
